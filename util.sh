7943b2ace9cd4903943e7031b4e53353db40dc82
kakiezhang <782922086@qq.com>
2021-11-24 00:23:02 +0800

    insert array

---
+++ util.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+insert_array () {
+	local ele=$1
+	local -n arr=$2
+	local pos=$3
+
+	if [ $pos -lt 0 ];then
+		return
+	fi
+
+	before_cnt=${#arr[@]}
+	if [ $pos -gt $before_cnt ];then
+		pos=$before_cnt
+	fi
+
+	arr+=(${ele})
+	after_cnt=${#arr[@]}
+	if [ $pos -eq $after_cnt ];then
+		return
+	fi
+
+	for ((i=$after_cnt-1; i>${pos}; i-=1))
+	do
+		tmp=${arr[$i]}
+		arr[$i]=${arr[$i-1]}
+		arr[$i-1]=$tmp
+	done
+}
+
+brr=(a)
+insert_array e brr 2
+declare -p brr
